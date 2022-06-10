ea35208a5d46e5e40edaa4832494b944512a6d5c
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-10 18:01:23 +0300

    commit tree eba9e01698a3430e585b55395ae96af41a3f00bd

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(destination) { 
+    let hQ= 42;
+    if (destination >= hQ) {
+        return destination - hQ;
+    }
+    else {
+        return hQ - destination
+}
+distanceFromHqInBlocks(43)
+distanceFromHqInBlocks(50)
+distanceFromHqInBlocks(34)
+}
+function distanceFromHqInFeet(destination) {
+    let hQ = 42;
+    if (destination >= hQ) {
+        return (destination - hQ)*264;
+    }
+    else{
+        return (hQ-destination)*264
+    }
+    
+}
+distanceFromHqInFeet(43)
+distanceFromHqInFeet(50)
+distanceFromHqInFeet(34)
+
+let distanceTravelledInFeet = function (a, b) {
+    if (a >= b) {
+        return (a-b)*264
+        
+    }
+    else{
+        return (b-a)*264
+    }
+    
+}
+distanceTravelledInFeet(43, 48)
+distanceTravelledInFeet(50, 60)
+distanceTravelledInFeet(34, 28)
+
+function calculatesFarePrice(start, destination) {
+    let dist = distanceTravelledInFeet(start, destination)
+    if (dist < 400) {
+        return 0
+    }
+    else if (dist < 2000) {
+        return (dist - 400)*0.02;
+        
+    }
+    else if (dist <=2500) {
+        return 25
+        
+    }
+    else{
+        return 'cannot travel that far';
+    }
+
+    
+    
+}
+calculatesFarePrice(43, 48)
+calculatesFarePrice(50,60)
+calculatesFarePrice(34, 28)
